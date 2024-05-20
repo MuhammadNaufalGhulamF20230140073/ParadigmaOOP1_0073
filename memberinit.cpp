2a136dd3657d2879625dcead079930564ba7b829
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-20 14:39:31 +0700

    membuat akses modifier public pada class pasien

---
+++ memberinit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class mahasiswa{
+    private:
+        const int id;
+        string nama;
+        float nilai;
+
+    public:
+        mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+        {
+
+        }
+
+        ~mahasiswa(){
+            cout << "id = " << id << endl;
+            cout << "Nama = " << nama << endl;
+            cout << "Nilai = " << nilai << endl;
+        }
+};
+
+int main(){
+    mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
+}
